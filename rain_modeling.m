f4c0499114167fda1f582503cd4d37d40dce8cdc
Charles Zhao <charleszhao94@gmail.com>
2026-05-09 20:26:21 -0700

    Delete rain_modeling.m

--- rain_modeling.m
+++
@@ -1,8 +0,0 @@
-latitude = netcdf.getVar(ncid, 0);
-longitude = netcdf.getVar(ncid, 1);
-time = netcdf.getVar(ncid, 2);
-data = netcdf.getVar(ncid, 3);
-
-netcdf.close(ncid);
-
-% Single value decomposition
